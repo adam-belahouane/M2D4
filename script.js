c00ba94c29975abe8b55fa5403c1b1671ee6b1a1
adam-belahouane <adam_belahouane@outlook.com>
2021-08-26 16:41:15 +0100

    teamgen update

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ const addName = function(){
 
 
 }
-const assignName = function(){
-    let name = document.querySelector("li")
-    
+const genTeams = function(){
+    let x = document.querySelector(".Teams")
+    x.innerHTML = ""
+    let numOfTeams = document.querySelector("#group-number").value
+     numOfTeams = parseInt(numOfTeams)
+     console.log(numOfTeams)
+    for(let i=1; i<=numOfTeams ;i++){
+        let parent = document.createElement("ul")
+        parent.classList.add("list-group")
+        parent.classList.add(i)
+
+        document.querySelector(".Teams").appendChild(parent)
+        
+    }
+
 }
